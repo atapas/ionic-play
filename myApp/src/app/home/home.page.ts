fbbe979b3f9fd330d16b27fb040df9f8465aae8f
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-09-09 23:30:46 +0530

    Dynamic loading of images and text

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -11,6 +11,27 @@ export class HomePage {
     initialSlide: 1,
     speed: 400
   };
-  constructor() {}
+
+  slideData: any = [];
+
+  constructor() {
+    this.slideData = [
+      {
+      'image': 'rajni_image.png',
+      'name': 'Rajni Sir',
+      'joke': "Once spiderman,superman and batman visited rajnikant's house together..it was teachers day!"
+      },
+      {
+        'image': 'chuckn_image.png',
+        'name': 'Chuck Norris',
+        'joke': "Chuck Norris will never have a heart attack. His heart isn't nearly foolish enough to attack him."
+      },
+      {
+        'image': 'jackiechan.png',
+        'name': 'Jackie Chan',
+        'joke': "If you watch a movie with Jackie Chan backwards...You will get a documentary about a Chinese guy who assembles furniture with his feet."
+      }
+    ]
+  }
 
 }

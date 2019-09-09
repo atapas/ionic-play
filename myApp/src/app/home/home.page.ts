575c1a3b076c1de7d416034678d26b3a319579b9
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-09-09 22:28:31 +0530

    Initial Commit

---
+++ myApp/src/app/home/home.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+  constructor() {}
+
+}
